117db17768f0a48dcc23186a45180c471b6b3e99
ClauberDev <clauber.dev@gmail.com>
2023-05-25 18:43:31 -0300

    first commit

---
+++ lib/ll.rb
@@ -0,0 +1,13 @@
+
+#You will need two classes:
+
+#LinkedList class, which will represent the full list.
+class LinkedList
+  
+end
+# Node class, containing a #value method and a link to the #next_node, 
+# set both as nil by default.
+class Node
+  
+end
+
---
+++ lib/node.rb
@@ -0,0 +1,4 @@
+# Node class, containing a #value method and a link to the #next_node, set both as nil by default.
+class Node
+
+end
